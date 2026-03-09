711af69d9c5d693bec4acb1031f3255537aa518c
Thalis Antunes <thalisantunessilva@gmail.com>
2026-03-09 12:15:37 -0300

    fix: edit atributes in DTO to snake_case

--- src/main/java/com/example/truckage/dtos/FreteDTO.java
+++ src/main/java/com/example/truckage/dtos/FreteDTO.java
@@ -1,6 +1,7 @@
 package com.example.truckage.dtos;
 
 import com.example.truckage.models.Frete;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -8,30 +9,63 @@ import java.time.LocalDateTime;
 public record FreteDTO(
         Integer freteId,
 
-        @NotNull Integer empresaId,
+        @NotNull 
+        @JsonProperty("empresa_id")  
+        Integer empresaId,
 
         Integer motoristaId,
 
+        @JsonProperty("data_prevista_entrega")  
         LocalDateTime dataPrevistaEntrega,
 
-        @NotBlank String origemEstado,
-        @NotBlank String origemCidade,
-        @NotBlank String destinoEstado,
-        @NotBlank String destinoCidade,
+        @NotBlank 
+        @JsonProperty("origem_estado")  
+        String origemEstado,
+        
+        @NotBlank 
+        @JsonProperty("origem_cidade") 
+        String origemCidade,
+        
+        @NotBlank 
+        @JsonProperty("destino_estado")  
+        String destinoEstado,
+        
+        @NotBlank 
+        @JsonProperty("destino_cidade")  
+        String destinoCidade,
 
-        @NotNull @DecimalMin("0.01") BigDecimal valorFrete,
+        @NotNull @DecimalMin("0.01") 
+        @JsonProperty("valor_frete")  
+        BigDecimal valorFrete,
 
+        @JsonProperty("precisa_lona")  
         Boolean precisaLona,
+        
+        @JsonProperty("produto_quimico") 
         Boolean produtoQuimico,
 
+        @JsonProperty("observacoes_motorista")  
         String observacoesMotorista,
 
+        @JsonProperty("veiculo_tracao")  
         String veiculoTracao,
+        
+        @JsonProperty("tipos_carreta")  
         String tiposCarreta,
+        
+        @JsonProperty("comprimento_carreta")  
         String comprimentoCarreta,
+        
+        @JsonProperty("numero_eixos")  
         String numeroEixos,
+        
+        @JsonProperty("configuracao_modelo")  
         String configuracaoModelo,
+        
+        @JsonProperty("tipo_carga")  
         String tipoCarga,
+        
+        @JsonProperty("observacoes_carga")  
         String observacoesCarga,
 
         Frete.StatusFrete status
